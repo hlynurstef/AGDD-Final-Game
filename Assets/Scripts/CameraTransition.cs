19e9330409f68b584320b7845ae308624680963c
Gudjon <gudjonss12@ru.is>
2018-03-06 20:04:21 +0000

    prototype of 3d transition

---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Sequence mySeq = DOTween.Sequence();
+		mySeq.Insert(1, Camera.main.transform.DOMove(new Vector3(183, 58, 265), 2));
+		mySeq.Insert(1, Camera.main.transform.DORotate(new Vector3(15, 90, 0), 2));
+		mySeq.PrependInterval(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/DimensionTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DimensionTrigger : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player") == true) {
+			Sequence mySeq = DOTween.Sequence();
+			mySeq.Insert(0, Camera.main.transform.DOMove(new Vector3(200, 18, 340), 2));
+			mySeq.Insert(0, Camera.main.transform.DORotate(new Vector3(10, 180, 0), 2));
+			mySeq.PrependInterval(1);
+
+			Camera.main.GetComponent<ThirdPersonOrbitCamBasic>().enabled = false;
+			player.GetComponent<BasicBehaviour>().inputEnabled = false;
+		}
+	}
+}

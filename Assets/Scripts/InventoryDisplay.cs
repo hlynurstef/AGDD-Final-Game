4df2817f2f76ce2e5e1f514e6f3b9832af4425ba
sigrung15 <sigrung15@ru.is>
2018-03-09 16:29:33 +0000

    made a bag be scriptable object and made objects in inventory dragable

---
+++ Assets/Scripts/InventoryDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplay : MonoBehaviour {
+
+	public InventoryItem inventoryItem;
+
+	public SpriteRenderer item;
+
+	// Use this for initialization
+	void Start () {
+		item.sprite = inventoryItem.item;
+	}
+}
---
+++ Assets/Scripts/ItemDragHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler {
+    
+	public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 1)
+		);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
+    }
+}

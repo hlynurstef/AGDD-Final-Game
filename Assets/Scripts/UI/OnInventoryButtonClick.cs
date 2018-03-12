c3eed814fe9458a6f2dfa9ed4df18e59c2d24712
sigrung15 <sigrung15@ru.is>
2018-03-12 13:28:15 +0000

    background and draggable objects from inventory, not finished

---
+++ Assets/Scripts/UI/OnInventoryButtonClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnInventoryButtonClick : MonoBehaviour {
+
+	public GameObject activated;
+	public GameObject deactivated;
+
+	void OnMouseDown() {
+		activated.SetActive(false);
+		deactivated.SetActive(true);
+	}
+
+	void OnMouseUp() {
+		print("hello");
+		activated.SetActive(true);
+		deactivated.SetActive(false);
+	}
+}

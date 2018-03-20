42ec146226b5e82f7c63a98c3e3b52e212b0339f
Guðjón <gudjonss12@ru.is>
2018-03-20 12:10:20 +0000

    Stairs animation available through the gamemanager, good stöff

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance = null;
+	private Transform forestStairs;
+
+	void Awake()
+	{
+		// Singleton pattern
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+	}
+
+	void Start()
+	{
+		// If it looks stupid but it works, it ain't stupid!
+		forestStairs = GameObject.FindWithTag("Stairs 1").transform;
+	}
+
+	void AnimateStairs()
+	{
+		Sequence mySeq = DOTween.Sequence();
+		mySeq.Append(forestStairs.DOMoveY(0, 5, false));
+		mySeq.Insert(0, forestStairs.DOShakePosition(mySeq.Duration(), 0.1f, 100, 90, false, true));
+		mySeq.PrependInterval(5.0f);
+	}
+}

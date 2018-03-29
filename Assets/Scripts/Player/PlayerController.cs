6346d0014bef1eda4ee62c6c0303326beb5ce420
Guðjón <gudjonss12@ru.is>
2018-03-29 15:44:59 +0000

    Player can no longer interact when airborne

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -135,7 +135,7 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     public void Interact()
     {
-        if (interactable != null)
+        if (interactable != null && controller.isGrounded == true)
         {
             interactable.Interact();
             velocity.x = 0;

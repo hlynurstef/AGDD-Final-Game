189dc6d9540c2bc0ca7064f1aad56552bbc9a9b0
sigrung15 <sigrung15@ru.is>
2018-03-31 13:40:17 +0000

    added light to torches and darkness to game. Also added stairs to first level, this is a seperate tilemap, so can be enabled

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using DG.Tweening;
 using Yarn.Unity;
 using Com.LuisPedroFonseca.ProCamera2D;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,13 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance = null;
     private Transform forestStairs;
     public bool playerFrozen = false;
+
+    public Tilemap DarkMap;
+    public Tilemap BlurredMap;
+    public Tilemap BackgroundMap;
+
+    public Tile DarkTile;
+    public Tile BlurredTile;
 
     void Awake()
     {
@@ -28,6 +36,17 @@ public class GameManager : MonoBehaviour
     {
         // If it looks stupid but it works, it ain't stupid!
         forestStairs = GameObject.FindWithTag("Stairs 1").transform;
+
+        DarkMap.origin = BlurredMap.origin = BackgroundMap.origin;
+        DarkMap.size = BlurredMap.size = BackgroundMap.size;
+
+        foreach (Vector3Int p in DarkMap.cellBounds.allPositionsWithin) {
+            DarkMap.SetTile(p, DarkTile);
+        }
+
+        foreach (Vector3Int p in BlurredMap.cellBounds.allPositionsWithin) {
+            BlurredMap.SetTile(p, BlurredTile);
+        }
     }
 
     [YarnCommand("build_stairs")]

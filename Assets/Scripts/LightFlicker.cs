f2d565c96c6ca75bdafb5ff4bb5cb2ed2dda4b81
sigrung15 <sigrung15@ru.is>
2018-03-31 13:58:48 +0000

    added one more light so that player will not see difference in tiles, and also made stair a ground layer. Added flicker to lights

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	[Range(0.05f, 0.2f)]
+	public float flickTime;
+
+	[Range(0.002f, 0.02f)]
+	public float addSize;
+
+	float timer = 0;
+	private bool bigger = true;
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (timer > flickTime) {
+			if (bigger) {
+				transform.localScale = new Vector3(transform.localScale.x + addSize, transform.localScale.y + addSize, transform.localScale.z);
+			}
+			else {
+				transform.localScale = new Vector3(transform.localScale.x - addSize, transform.localScale.y - addSize, transform.localScale.z);
+			}
+
+			timer = 0;
+			bigger = !bigger;
+		}
+	}
+}

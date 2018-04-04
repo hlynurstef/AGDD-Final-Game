989938c2e1b2403f364c6906708532ff60bf27dc
Hlynur Stefansson <hlynurs15@ru.is>
2018-04-04 16:30:26 +0000

    Make woodsman sweat 0.1% of the time when chopping

--- Assets/Scripts/Woodsman.cs
+++ Assets/Scripts/Woodsman.cs
@@ -3,35 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class Woodsman : NPC {
+public class Woodsman : NPC
+{
 
-	private bool hasWholeAxe = false;
-	private float chopTimer = 0.0f;
-	[SerializeField]
-	private GameObject woodContainer;
+    [SerializeField]
+    private GameObject woodContainer;
 
-	[SerializeField]
-	private const float addWoodInterval = 120.0f;
-	
-	void Update ()
-	{
-		if (hasWholeAxe == true)
-		{
-			chopTimer += Time.deltaTime;
-			if (chopTimer >= addWoodInterval)
-			{
-				woodContainer.GetComponent<Container>().AddItem(ItemType.Wood, 5);
-				chopTimer = 0.0f;
-	
-				string variableName = "$woodcontainer_amount";
-				FindObjectOfType<VariableStorage>().SetValue(variableName, new Yarn.Value(woodContainer.GetComponent<Container>().GetCount()));
-			}
-		}
-	}
+    [SerializeField]
+    private const float addWoodInterval = 120.0f;
 
-	[YarnCommand("chop")]
-	public void StartChopping()
-	{
-		hasWholeAxe = true;
-	}
+
+    private bool hasWholeAxe = false;
+    private float chopTimer = 0.0f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (hasWholeAxe == true)
+        {
+            chopTimer += Time.deltaTime;
+            if (chopTimer >= addWoodInterval)
+            {
+                woodContainer.GetComponent<Container>().AddItem(ItemType.Wood, 5);
+                chopTimer = 0.0f;
+
+                string variableName = "$woodcontainer_amount";
+                FindObjectOfType<VariableStorage>().SetValue(variableName, new Yarn.Value(woodContainer.GetComponent<Container>().GetCount()));
+            }
+
+            if (Random.Range(0f, 1f) < 0.001f)
+            {
+                animator.SetTrigger("isSweating");
+            }
+        }
+    }
+
+    [YarnCommand("chop")]
+    public void StartChopping()
+    {
+        hasWholeAxe = true;
+        animator.SetBool("isChopping", true);
+    }
 }

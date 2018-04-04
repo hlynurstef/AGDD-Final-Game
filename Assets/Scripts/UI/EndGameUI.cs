463b2ac27ffabdbbc8b8ad8022bda71583f57100
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2018-04-04 23:42:06 +0000

    End scene ugly2

---
+++ Assets/Scripts/UI/EndGameUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameUI : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        else if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+}

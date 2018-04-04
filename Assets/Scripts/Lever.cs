0e72d119facb087768e1ca1f150009e1c34fa077
Guðjón <gudjonss12@ru.is>
2018-04-04 15:48:22 +0000

    Created prefab for Lever

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Lever : InteractableBase
+{
+	[SerializeField]
+	private Sprite[] sprites;
+	private SpriteRenderer spriteRenderer;
+	private int currentSpriteIndex;
+	
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		currentSpriteIndex = 0;
+	}
+
+	[YarnCommand("flip")]
+	public void FlipLever()
+	{
+		print("Flipping");
+		currentSpriteIndex = (currentSpriteIndex == 1) ? 0 : 1;
+		spriteRenderer.sprite = sprites[currentSpriteIndex];
+	}
+}
